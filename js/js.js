1caf41961ad145a420499a96f1f8a94237adcc32
Florian Köhler <Florian.Koehler@gmx.net>
2018-05-20 18:21:13 +0200

    Added function for adding / removing timer boxes

--- js/js.js
+++ js/js.js
@@ -1,6 +1,9 @@
 $(document).ready(function() {
     getWeather(); //Get the initial weather.
     setInterval(getWeather, 600000); //Update the weather every 10 minutes.
+
+    $('#add-timer').click(Add_new_timer);
+    $('.remove').click(Remove_timer);
   });
   
   function getWeather() {
@@ -8,15 +11,24 @@ $(document).ready(function() {
       location: 'Haar, Munich, Germany',
       unit: 'c',
       success: function(weather) {
-        html = '<h2><i class="icon-'+weather.code+'"></i> '+weather.temp+'&deg;'+weather.units.temp+'</h2>';
-        html += '<ul><li>'+weather.city+', '+weather.region+'</li>';
-        html += '<li class="currently">'+weather.currently+'</li>';
-        html += '<li>'+weather.wind.direction+' '+weather.wind.speed+' '+weather.units.speed+'</li></ul>';
+        html = '<h2><i class="icon-' + weather.code + '"></i> ' + weather.temp + '&deg;' + weather.units.temp + '</h2>';
+        html += '<ul><li>' + weather.city + '</li>';
+        html += '<li class="currently">' + weather.currently + '</li>';
+        html += '<li>' + weather.wind.speed + ' ' + weather.units.speed + '</li></ul>';
     
         $("#weather").html(html);
       },
       error: function(error) {
-        $("#weather").html('<p>'+error+'</p>');
+        $("#weather").html('<p>' + error + '</p>');
       }
     });
+  }
+
+  function Add_new_timer() {
+    $('#timer_box').append('<div class="timer"><div class="titel">Blumen gießen</div><div class ="counter">20:00:00</div><div class="remove">X</div></div>');
+    $('.remove').click(Remove_timer);
+}
+
+  function Remove_timer() {
+    $(this).parent().remove();
   }
